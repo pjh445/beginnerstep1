b74bc0bc5b6328ebb490720f2213b2849fbc6ad1
pjh445 <pjh445@gmail.com>
2019-03-08 10:18:30 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
+var express = require("express");
+var app = express();
+
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
-http.createServer(function(request,response){
+var app=http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var title = queryData.id;
-	
 		
 	fs.readdir('./data',function(error,filelist){
 				//console.log(filelist);//배열오브젝트반환됨	
 				
 		fs.readFile(`data/${title}`,'utf8',	function(err,description){
 			
-				if(title===undefined){
-							title="Dynamic Web";
-							description="Node.js를 이용한 다이나믹 웹 만들기"; 
+				if(title==undefined || title == null){
+					title="Dynamic Web";
+					description='<p id="main">Node.js를 이용한 다이나믹 웹 만들기<br><br><img src="https://1.bp.blogspot.com/-qEnMk64JS24/VqyoIgdkWGI/AAAAAAAAELk/4zMqL-kbClg/s1600/best-web-browser.jpg" alt="WEB">dddd</p>'; 
 				}
 				
 				var list='<ul>';	
@@ -35,21 +37,49 @@ http.createServer(function(request,response){
 <head>
 <title>WEB1 - ${title}</title>
 <meta charset="utf-8">
+
+<style>
+a {text-decoration:none; color:black; padding:2px;}
+nav ul {list-style:none; display:flex;}
+nav li {flex:1; text-align:center;margin:5px;}
+nav a {display:block; width:100%; transform:scale(1.2) translateY(-2px); font-weight:bold;}
+nav a:hover, .active {background:gold;}
+img {display:block; margin:auto; width:70%;}
+#layout {width:60%; margin:auto;}
+#main {background: skyblue; padding:10px;}
+</style>
 </head>
 <body>
-<h1><a href="/">WEB</a></h1>
-${list}				  
-<h2>${title}</h2>
-<p>${description}</p>
-<img src="https://1.bp.blogspot.com/-qEnMk64JS24/VqyoIgdkWGI/AAAAAAAAELk/4zMqL-kbClg/s1600/best-web-browser.jpg" alt="WEB">
+<div id="layout">
+	<h1><a href="/">WEB</a></h1>
+	<nav>${list}</nav>				  
+	<h2>${title}</h2>
+	${description}
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+	var txt= $("h2").text();
+	var num=0;
+	switch(txt){
+		//case 'html':		num=0;break;
+		case 'css':			num=1;break;
+		case 'javascript':	num=2;break;
+		case 'noejs':		num=3;
+	}
+	$("nav li").eq(num).children().addClass("active");
+});
+</script>
 </body>
 </html>`;
 /*************/
 						
-						response.writeHead(200);
-						response.write(template);
-						response.end();
+			response.writeHead(200);
+			response.write(template);
+			response.end();
 		});
 	}); 
 
-}).listen(3000);
+});
+var PORT = process.env.PORT;
+app.listen(PORT)
